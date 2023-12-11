3890ab9a488ba278fffbc89cc6845b3f511fc463
Stephan <112871518+spacejump3@users.noreply.github.com>
2023-12-11 18:24:30 +0100

    folder structure / sveltekit + d3 setup

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Testcomponent from '../components/Testcomponent.svelte';
+</script>
+
+<body>
+    <h1>Welcome to SvelteKit</h1>
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+
+    <Testcomponent />
+</body>
