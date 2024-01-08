0fc01722690f08922bbe5fb3fcb3475e673a6d84
yelunl <yeliz.unlu@hva.nl>
2024-01-08 12:07:16 +0100

    group years dataset

--- src/components/Testcomponent.svelte
+++ src/components/Testcomponent.svelte
@@ -1,6 +1,14 @@
 <script>
-    import { onMount } from 'svelte';
-    import * as d3 from 'd3';
-</script>
+    import { onMount } from "svelte";
+    import * as d3 from "d3";
+    import TweedeKamer_perioden from '../TweedeKamer_perioden.json';
 
-test
+    onMount(async () => {
+        const beginjaar = d3.group(TweedeKamer_perioden, d => d["Beginjaar"]);
+        console.log(beginjaar);
+
+        const eindjaar = d3.group(TweedeKamer_perioden, d => d['Eindjaar']);
+        console.log(eindjaar);
+
+    });
+</script>
