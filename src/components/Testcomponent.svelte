6cd1b63b1d697cdbb9fbf9229e853892722cf6b0
yelunl <yeliz.unlu@hva.nl>
2024-01-10 15:15:07 +0100

    create start treemap

--- src/components/Testcomponent.svelte
+++ src/components/Testcomponent.svelte
@@ -1,27 +1,27 @@
 <script>
-    import { onMount } from 'svelte';
-    import * as d3 from 'd3';
+    import { onMount } from "svelte";
+    import * as d3 from "d3";
 
     onMount(async () => {
-        const data = await d3.json('TweedeKamer_perioden.json');
+        const data = await d3.json("TweedeKamer_perioden.json");
         data.forEach((item) => (item.value = 1));
 
         // Iterate through each object in the array and rename the key
         for (var i = 0; i < data.length; i++) {
-            if ('Naam (Uit personen))' in data[i]) {
+            if ("Naam (Uit personen))" in data[i]) {
                 // Create a new key and assign the value of the old key
-                data[i].Naam = data[i]['Naam (Uit personen))'];
+                data[i].Naam = data[i]["Naam (Uit personen))"];
                 // Delete the old key
-                delete data[i]['Naam (Uit personen))'];
+                delete data[i]["Naam (Uit personen))"];
             }
         }
 
         // Group by Beginjaar and Beginmaand
         const groupedData = d3.group(
             data,
-            (d) => Math.floor((parseInt(d['Beginjaar'], 10) - 1815) / 16), // Group every 16 years starting from 1815
-            (d) => d['Beginjaar'],
-            (d) => d['Beginmaand']
+            (d) => Math.floor((parseInt(d["Beginjaar"], 10) - 1815) / 16), // Group every 16 years starting from 1815
+            (d) => d["Beginjaar"],
+            (d) => d["Beginmaand"],
         );
 
         // Convert the map to an array of objects and sort by Beginjaar
@@ -36,14 +36,14 @@
                                 Beginmaand: parseInt(beginMonth, 10),
                                 children: persons,
                             };
-                        }
+                        },
                     ).sort((a, b) => a.Beginmaand - b.Beginmaand);
 
                     return {
                         Beginjaar: parseInt(beginYear, 10),
                         children: sortedMonthsData,
                     };
-                }
+                },
             ).sort((a, b) => a.Beginjaar - b.Beginjaar);
 
             return {
@@ -64,38 +64,38 @@
                         const personsFromCurrentMonth = children.filter(
                             (person) => {
                                 return (
-                                    parseInt(person['Beginjaar'], 10) ===
+                                    parseInt(person["Beginjaar"], 10) ===
                                         Beginjaar &&
-                                    parseInt(person['Beginmaand'], 10) ===
+                                    parseInt(person["Beginmaand"], 10) ===
                                         Beginmaand
                                 );
-                            }
+                            },
                         );
 
                         // Include persons from previous years and months if their ending year is equal to or later than the current year and month
                         accumulatedPersons = accumulatedPersons.filter(
                             (person) => {
                                 return (
-                                    parseInt(person['Eindjaar'], 10) >
+                                    parseInt(person["Eindjaar"], 10) >
                                         Beginjaar ||
-                                    (parseInt(person['Eindjaar'], 10) ===
+                                    (parseInt(person["Eindjaar"], 10) ===
                                         Beginjaar &&
-                                        parseInt(person['Eindmaand'], 10) >=
+                                        parseInt(person["Eindmaand"], 10) >=
                                             Beginmaand)
                                 );
-                            }
+                            },
                         );
 
                         // Add persons from the current year and month
                         accumulatedPersons = accumulatedPersons.concat(
-                            personsFromCurrentMonth
+                            personsFromCurrentMonth,
                         );
 
                         return {
                             Beginmaand,
                             children: accumulatedPersons.slice(), // Copy the accumulatedPersons array to avoid reference issues
                         };
-                    }
+                    },
                 );
 
                 return {
@@ -110,7 +110,71 @@
             };
         });
 
-        const finalResult = { year: 'root', children: tweedeKamerData };
-        console.log(finalResult);
+        const finalResult = { year: "root", children: tweedeKamerData };
+        // console.log(finalResult);
+
+        const root = d3.hierarchy(finalResult);
+        root.sum((d) => d.value);
+
+        const layout = d3
+            .treemap()
+            .size([1500, 800])
+            .padding(10)
+            // .tile(d3.treemapDice);
+
+        layout(root);
+
+        const x = d3.scaleLinear()
+        .rangeRound([0, 1500])
+        .domain([0, 1500])
+
+        const y = d3.scaleLinear()
+        .rangeRound([0, 800])
+        .domain([0, 800])
+
+        // const func = (data) => {
+            const visualisation = d3
+            .select("svg")
+            .selectAll("g")
+            .data(root.children)
+            .join("g");
+
+        // Create rectangulars
+        visualisation
+            .append("rect")
+            .attr("fill", "transparent")
+            .attr("stroke", "black")
+            .attr("x", function (d) {
+                return x(d.x0);
+            })
+            .attr("y", function (d) {
+                return y(d.y0);
+            })
+            .attr("width", function (d) {
+                return x(d.x1) - x(d.x0);
+            })
+            .attr("height", function (d) {
+                return y(d.y1) - y(d.y0);
+            })
+            .on('click', (d, e) => zoom(d))
+
+        // create text
+        visualisation
+            .append("text")
+            .text((d) => d.data["Group"])
+            .attr("x", (d) => d.x0 + 10)
+            .attr("y", (d) => d.y0 + 20);
+
+        const zoom = (d) => {
+            x.domain([d.x0, d.x1])
+            y.domain([d.y0, d.y1])
+        }
+
+
+
     });
 </script>
+
+<section>
+    <svg width="1500" height="1000"> </svg>
+</section>
